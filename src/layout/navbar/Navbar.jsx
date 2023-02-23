6ba4100067f530ddd7afe42349d776aa35be153a
PradeepR-proponent <pradeep.r@proponenttechnologies.com>
2023-02-23 10:52:55 +0530

    navbar link color changed

--- src/layout/navbar/Navbar.jsx
+++ src/layout/navbar/Navbar.jsx
@@ -12,25 +12,25 @@ const Navbar = () => {
       Path: pageRoutes.homePage,
       Id: 1,
       LinkName: "Home",
-      Icon: <AiFillHome color="#ffffff" size={20} />
+      Icon: <AiFillHome color="rgba($color: #09e6ff70, $alpha: 2.0)" size={20} />
     },
     {
       Path: pageRoutes.aboutUs,
       Id: 2,
       LinkName: "About Me",
-      Icon: <FaUserAlt color="#ffffff" size={20} />
+      Icon: <FaUserAlt color="rgba($color: #09e6ff70, $alpha: 2.0)" size={20} />
     },
     {
       Path: pageRoutes.portfolio,
       Id: 3,
       LinkName: "Portfolio",
-      Icon: <AiFillHeart color="#ffffff" size={20} />
+      Icon: <AiFillHeart color="rgba($color: #09e6ff70, $alpha: 2.0)" size={20} />
     },
     {
       Path: pageRoutes.contact,
       Id: 4,
       LinkName: "Contact Me",
-      Icon: <MdPermContactCalendar color="#ffffff" size={20} />
+      Icon: <MdPermContactCalendar color="rgba($color: #09e6ff70, $alpha: 2.0)" size={20} />
     },
 
   ]
@@ -38,7 +38,8 @@ const Navbar = () => {
     <header className='navbar'>
       <ul className='navLink'>
         {
-          links.map((l) => <li><NavLink style={({ isActive }) =>isActive ? { borderBottom: "2px solid #ffffff" } : undefined} to={l.Path}>{l.Icon}&nbsp;&nbsp;{l.LinkName}</NavLink></li>)
+          links.map((l) => <li><NavLink className={({isActive})=> isActive ?  "activeClass" :""}
+          to={l.Path}>{l.Icon}&nbsp;&nbsp;{l.LinkName}</NavLink></li>)
         }
       </ul>
     </header>
--- src/pagesComponent/homepage/UserSection.jsx
+++ src/pagesComponent/homepage/UserSection.jsx
@@ -4,13 +4,12 @@ const UserSection = () => {
   return (
     <div className='userSection'>
       <div className='userBackground'>
-        <img src={user} alt=""  />
-        <div className='userName'>
+        <img src={user} alt="" />
+      </div>
+      <div className='userName'>
         <p>john doe</p>
         <h1>MERN Developer</h1>
-        </div>
       </div>
-
     </div>
   )
 }
