29511e35341fce95899b7024c157c3db1b40beca
Pradeep Rajput <pradeep87055@gmail.com>
2023-06-02 22:53:26 +0530

    profolio done

--- src/pagesComponent/portfolio/Portfolio.jsx
+++ src/pagesComponent/portfolio/Portfolio.jsx
@@ -3,6 +3,67 @@ import React from 'react'
 import { iconsData } from '../../data/constant';
 import { AiOutlineSend } from "react-icons/ai";
 
+
+const workingProjects = [
+    {
+        project: "Yogshala.in",
+        description: "Yogshala.in is a comprehensive social media platform designed specifically for yoga teachers and students. This project encompasses both a web application and a mobile app that communicate with APIs. The platform offers a range of features, including chat functionality for seamless communication, yoga class scheduling for convenient planning, and addressing health issues related to yoga. By providing a dedicated space for the yoga community, Yogshala.in aims to connect individuals, facilitate learning, and promote overall well-being through the practice of yoga.",
+        technology: "(Nextjs, React-Native, Nodejs, Expressjs and Mongodb)"
+    },
+    {
+        project: "Beatles Choice",
+        description: "Beatles Choice is an e-commerce website designed to efficiently manage and sell a wide range of products across various categories. The website provides a user-friendly interface for customers to browse and purchase products based on their preferences. With robust categorization and organization, Beatles Choice aims to enhance the shopping experience by ensuring easy navigation and streamlined product selection.",
+        technology: "(Nextjs, Nodejs, Expressjs and Mongodb)"
+    },
+]
+
+const projectData = [
+    {
+        project: "UC Toolbot",
+        description: "Responsibilities:Fixed bugs in functionality and UI for improved user experience. Collaborated with the team to identify and address issues, implementing effective solutions. Assisted in testing and troubleshooting to ensure the overall quality of the application was maintained.",
+        technology: "(React-Electron Js)"
+    },
+    {
+        project: "Argon AIO",
+        description: "Developed Argon AIO from scratch using React-Electron Js, incorporating state management with Redux and react-redux. Successfully created a robust application with seamless user experiences, ensuring efficient state management and enhancing overall performance.",
+        technology: "(React-Electron Js)"
+    },
+    {
+        project: "KYRO Tools",
+        description: "I played a key role in developing the user interface, ensuring a visually appealing and intuitive design. I also implemented a dark mode feature to enhance user customization and experience. Additionally, I created a website using React.js, showcasing the project and providing relevant information in an engaging manner.",
+        technology: "(React-Electron Js and React)"
+    },
+    {
+        project: "Truckindigital",
+        description: "In my work on the Truckindigital project, I was responsible for creating the user interface, delivering an intuitive and visually appealing design. I also integrated various APIs to enable seamless data exchange and functionality within the application. Additionally, I developed a chat application using Getstream.io, allowing real-time communication and collaboration among users.",
+        technology: "(React Js)"
+    },
+    {
+        project: "Sonu Haircut salon",
+        description: "In my role at Sonu Haircut Salon, I successfully developed two apps using React Native Expo. First, I created a staff app that streamlined scheduling, resource management, and enhanced operational efficiency for the salon. Additionally, I developed a client app that provided users with a seamless and user-friendly experience for booking appointments and accessing salon services.",
+        technology: "(React-Native Expo)"
+    },
+    {
+        project: "Farmpreneur",
+        description: "In the Farmpreneur app, I took charge of designing the user interface and integrating APIs to provide weather information from open weather services. This app provided a platform for farmers to sell their crops, empowering them with a digital marketplace to reach potential buyers and optimize their sales opportunities.",
+        technology: "(React-Native Expo)"
+    },
+    {
+        project: "Vietnam Factory ",
+        description: "In my contributions to the Vietnam Factory B2B project, I successfully addressed bugs and errors in the frontend APIs during version 1, ensuring smooth functionality. In version 2, I took on a more comprehensive role by creating the project from scratch. This involved setting up the entire project using Nuxt.js, implementing robust state management, and incorporating authentication through Nuxt plugins. I also facilitated language translation in both Vietnamese and English, enhancing accessibility. Furthermore, I integrated Laravel APIs to seamlessly fetch and display data on the frontend, improving overall user experience.",
+        technology: "(Nuxtjs)"
+    },
+]
+
+
+
+
+
+
+
+
+
+
 const Portfolio = () => {
     return (
         <div className='portfolioSection'>
@@ -21,10 +82,31 @@ const Portfolio = () => {
                 </div>
             </div>
             <div className='projects'>
+
                 <div className='iconContainer'>
-                    {iconsData.map((i, idx) => <div key={idx} className='iconBox'> {i.icon}
-                        <p className='iconName' >{i.name}</p>
-                    </div>)}
+                    <h2 className='h1'>Work Experience</h2>
+                    {
+                        projectData.map((d) => (
+                            <div className='second-work'>
+                                <p className='text'><AiOutlineSend /> {d.project}</p>
+                                <p className='lightText'>{d.technology}</p>
+                                <p className='bigPara'>{d.description}
+                                </p>
+                            </div>
+                        ))
+                    }
+
+<h2 className='h1'>Working Projects</h2>
+{
+                        workingProjects.map((d) => (
+                            <div className='second-work'>
+                                <p className='text'><AiOutlineSend /> {d.project}</p>
+                                <p className='lightText'>{d.technology}</p>
+                                <p className='bigPara'>{d.description}
+                                </p>
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
         </div>
