bd62c1635569ca38a6f4b6a4e381fdf6aff413bd
Pradeep Rajput <pradeep87055@gmail.com>
2023-06-04 21:40:00 +0530

    education added

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -3,7 +3,7 @@ import { SectionWrapper, ContentWrapper } from '../../layout'
 import { loadFull } from "tsparticles";
 import { Particle } from '../../components';
 import { hompageOptions } from '../../data/particles'
-import { UserSection, Skills, PortfolioSection, ContactSection } from '../../pagesComponent';
+import { UserSection, Skills, PortfolioSection, ContactSection,MyEducation } from '../../pagesComponent';
 
 const HomePage = () => {
 
@@ -21,6 +21,7 @@ const HomePage = () => {
                 <ContentWrapper>
                     <Skills />
                     <PortfolioSection />
+                    <MyEducation/>
                     <ContactSection />
                 </ContentWrapper>
             </SectionWrapper>
