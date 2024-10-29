77d76d3d14972ff88b1a16cbb17318466c6d2671
DharmendraD87 <83401736+Pradeep87@users.noreply.github.com>
2024-10-29 19:06:47 +0530

    Update UserSection.jsx

--- src/pagesComponent/homepage/UserSection.jsx
+++ src/pagesComponent/homepage/UserSection.jsx
@@ -10,8 +10,7 @@ const UserSection = () => {
         <p className='name' > <span>I'm</span> Pradeep</p>
         <svg viewBox="0 0 1320 300">
           <text x="48%" y="28%" textAnchor="middle">
-             Software Development Enjeener
-          </text>
+Software Development Engineer          </text>
         </svg>
         <p className='subtext' >
         I’m a dynamic Software Development Engineer with over 2 years of experience in development, specializing in React.js, React Native, and Electron.js. I excel at building high-performance applications, integrating complex APIs, and resolving intricate bugs. I have a proven track record in delivering innovative solutions for Army projects, food apps, and B2B platforms.
